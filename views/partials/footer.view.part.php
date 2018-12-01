b131a654574ec2b43a5d4d11f8d27462db1a7d8e
jorbencas <jorbencas@gmail.com>
2018-12-01 17:41:29 +0100

    Adding boostrap template

---
+++ views/partials/footer.view.part.php
@@ -0,0 +1,5 @@
+<footer>
+    El propisote de este proyecto es la de mediante una tabla creada en boostrap, mostrar información en una base de datos en  sql
+</footer>
+</body>
+</html>
---
+++ views/partials/header.view.part.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/bootstrap.min.js"></script>
+    <style>
+        body{
+            color:white;
+            background-color:#32383e;
+        }
+        h1{
+            text-align:center;
+            padding-bottom:3%;
+        }
+        footer {
+            padding:2%;
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #212529;
+   color: white;
+   text-align: center;
+}
+    </style>
+</head>
+<body>
+<h1>Concesionario</h1>
